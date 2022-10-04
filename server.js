82a45e7294ca2a550664c171f5f6e4aa60c44241
sanjay-19-04 <sg943884@gmail.com>
2022-10-04 22:54:38 +0530

    ()

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL
     ,{ useNewUrlParser: true })
 
-const db = mongoose.connection
+const db = mongoose.connection()
 db.on('error',error => console.error(error))
 db.once('open', error => console.log('Connected to MongoDB'))
 
