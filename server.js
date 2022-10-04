0ea4a5985b87b93f06b6294e1f77726f005cf008
sanjay-19-04 <sg943884@gmail.com>
2022-10-04 22:39:21 +0530

    changed process

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL
+mongoose.connect("mongodb://localhost/mybrary"
     ,{ useNewUrlParser: true })
 
 const db = mongoose.connection
