f23b258c766ffe924cb3b58803a9c1b1760c7536
sanjay-19-04 <sg943884@gmail.com>
2022-10-04 21:34:41 +0530

    DATABAS - MONGO

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{
+mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
