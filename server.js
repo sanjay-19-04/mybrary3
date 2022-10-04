3ae2384e4a1930f3f5fd41325e06cebff8ca1f26
sanjay-19-04 <sg943884@gmail.com>
2022-10-04 21:29:39 +0530

    useUnifiedTopology: true added

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.static('public'))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 const db = mongoose.connection
 db.on('error',error => console.error(error))
