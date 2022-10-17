0ec9907c3bee7b565f20bb917a824ee039c44627
sanjay-19-04 <sg943884@gmail.com>
2022-10-17 11:39:13 +0530

    9 changes

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ if (process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app =express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
+
+
 const indexRouter = require('./views/index')
+const authorRouter = require('./routes/authors')
+
+
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb',extended:false}))
 
 const mongoose = require('mongoose')
 //please dont use process.env.DATABASE_URL,just copy and paste the url in the mongoose.connect and in 
@@ -24,5 +32,6 @@ db.once('open', error => console.log('Connected to MongoDB'))
 
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 app.listen(process.env.PORT || 3000)
